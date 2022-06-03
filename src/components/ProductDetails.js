57af487dd9ae4ac2069c829836e9f98afa41a67e
nmso2 <nmsubho@gmail.com>
2022-06-03 11:04:23 +0600

    completed basic structure

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,8 +1,41 @@
-import { Typography } from "@mui/material";
-import { Box } from "@mui/system";
 import React, { useEffect } from "react";
+import {
+  Typography,
+  Box,
+  Grid,
+  Link,
+  Breadcrumbs,
+  ImageList,
+  ImageListItem,
+  Paper,
+} from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProduct } from "../redux/slices/productSlice";
+
+const breadcrumbs = [
+  <Link
+    underline="hover"
+    key="1"
+    color="inherit"
+    href="/"
+    onClick={console.info("You clicked a breadcrumb.")}
+  >
+    MUI
+  </Link>,
+  <Link
+    underline="hover"
+    key="2"
+    color="inherit"
+    href="/material-ui/getting-started/installation/"
+    onClick={console.info("You clicked a breadcrumb.")}
+  >
+    Core
+  </Link>,
+  <Typography key="3" color="text.primary">
+    Breadcrumb
+  </Typography>,
+];
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product.product);
@@ -17,6 +50,44 @@ const ProductDetails = () => {
   return (
     <Box>
       <Typography>This is product details component!</Typography>
+
+      <Box sx={{ display: { sm: "flex" } }}>
+        <Typography sx={{ mr: 5 }}>Related Category</Typography>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          {breadcrumbs}
+        </Breadcrumbs>
+      </Box>
+
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Box sx={{ border: "1px solid red", height: "400px" }}>
+            <ImageList cols={2}>
+              {product?.gallery?.map((item) => (
+                <ImageListItem key={item?.url}>
+                  <img src={item?.url} alt={item.title} loading="lazy" />
+                </ImageListItem>
+              ))}
+            </ImageList>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box sx={{ border: "1px solid red", height: "400px" }}>
+            <Typography>{product?.title}</Typography>
+            <Paper elevation={3} sx={{ mt: 2, py: 3 }}>
+              <Typography>Price: 100</Typography>
+            </Paper>
+            <Paper elevation={3} sx={{ mt: 2, py: 3 }}>
+              <Typography>Color: Black</Typography>
+            </Paper>
+            <Paper elevation={3} sx={{ mt: 2, py: 3 }}>
+              <Typography>Size: 44</Typography>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
